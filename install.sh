fae3a76ab3e477e2e24f012781051629889af507
Natsuo Tsuji <n.tsuji@gmail.com>
2013-01-07 10:43:32 +0900

    Correct compatibility with Cygwin

--- install.sh
+++ install.sh
@@ -2,20 +2,15 @@
 
 cd "`dirname "$0"`"
 
-for dotfile in `cat .gitignore | grep '^!\.' | grep -v '^!\.git' | sed 's/^!//g'`
+for dotfile in `cat .gitignore | grep '^!\.' | grep -v '^!\.git' | sed 's/^!//g' | sed 's/\/$//g'`
 do
 	if [ -e "$HOME/$dotfile" -a ! -e "$HOME/$dotfile.bak" \
-		-a "`ls -al "$HOME" | fgrep " $dotfile " | sed 's/.*-> \(.*\)/\1/'`" != "$PWD/$dotfile" ]
+		-a "`ls -al "$HOME" | fgrep " $dotfile " | sed 's/.*-> \(.*\)/\1/' | sed 's/\/$//'`" != "$PWD/$dotfile" ]
 	then
 		mv "$HOME/$dotfile" "$HOME/$dotfile.bak"
 	else
 		rm -rf "$HOME/$dotfile"
 	fi
 
-	if [ -z "$CYGWIN" ]
-	then
-		ln -s "$PWD/$dotfile" "$HOME"
-	else
-		runas /user:administrator "mklink /d "`cygpath -w "$HOME/$dotfile"`" "`cygpath -w "$PWD/$dotfile"`""
-	fi
+	ln -s "$PWD/$dotfile" "$HOME"
 done
--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,14 @@
 
 cd "`dirname "$0"`"
 
-for dotfile in `cat .gitignore | grep '^!\.' | grep -v '^!\.git' | sed 's/^!//g'`
+for dotfile in `cat .gitignore | grep '^!\.' | grep -v '^!\.git' | sed 's/^!//g' | sed 's/\/$//g'`
 do
-	if [ "`ls -al "$HOME" | fgrep " $dotfile " | sed 's/.*-> \(.*\)/\1/'`" = "$PWD/$dotfile" ]
+	if [ "`ls -al "$HOME" | fgrep " $dotfile " | sed 's/.*-> \(.*\)/\1/' | sed 's/\/$//'`" = "$PWD/$dotfile" ]
 	then
-		rm -rf "$HOME/$dotfile"
-		mv "$HOME/$dotfile.bak" "$HOME/$dotfile"
+		rm "$HOME/$dotfile"
+		if [ -e "$HOME/$dotfile.bak" ]
+		then
+			mv "$HOME/$dotfile.bak" "$HOME/$dotfile"
+		fi
 	fi
 done
