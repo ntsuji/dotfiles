f26cd8a25e97a097c54b7d1c3d1e923b2794abf9
Natsuo Tsuji <n.tsuji@gmail.com>
2015-07-15 23:40:55 +0900

    Follow the Google Shell Style Guide

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-cd "`dirname "${0}"`"
+cd "$(dirname "${0}")"
 
-for symlink in `ls | grep "\.symlink$"`
-do
-	dotfile="`echo ${symlink} | sed 's/\(.*\)\.symlink/.\1/'`"
-
-	src="${PWD}/${symlink}"
-	dst="${HOME}/${dotfile}"
-	backup="${HOME}/${dotfile}.bak"
-
-	if [ -e "${dst}" -a ! -e "${backup}" -a "`readlink "${dst}"`" != "${src}" ]
-	then
-		mv "${dst}" "${backup}"
-	else
-		rm -rf "${dst}"
-	fi
-
-	ln -s "${src}" "${dst}"
+for symlink in $(ls | grep "\.symlink$"); do
+  readonly dotfile="$(echo ${symlink} | sed 's/\(.*\)\.symlink/.\1/')"
+  readonly src="${PWD}/${symlink}"
+  readonly dst="${HOME}/${dotfile}"
+  readonly backup="${HOME}/${dotfile}.bak"
+  if [[ -e "${dst}" &&
+      ! -e "${backup}" &&
+      "$(readlink "${dst}")" != "${src}" ]]; then
+    mv "${dst}" "${backup}"
+  else
+    rm -rf "${dst}"
+  fi
+  ln -s "${src}" "${dst}"
 done
--- uninstall.sh
+++ uninstall.sh
@@ -1,21 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-cd "`dirname "${0}"`"
+cd "$(dirname "${0}")"
 
-for symlink in `ls | grep "\.symlink$"`
-do
-	dotfile="`echo ${symlink} | sed 's/\(.*\)\.symlink/.\1/'`"
-
-	src="${PWD}/${symlink}"
-	dst="${HOME}/${dotfile}"
-	backup="${HOME}/${dotfile}.bak"
-
-	if [ "`readlink "${dst}"`" = "${src}" ]
-	then
-		rm "${dst}"
-		if [ -e "${backup}" ]
-		then
-			mv "${backup}" "${dst}"
-		fi
-	fi
+for symlink in $(ls | grep "\.symlink$"); do
+  readonly dotfile="$(echo ${symlink} | sed 's/\(.*\)\.symlink/.\1/')"
+  readonly src="${PWD}/${symlink}"
+  readonly dst="${HOME}/${dotfile}"
+  readonly backup="${HOME}/${dotfile}.bak"
+  if [[ "$(readlink "${dst}")" == "${src}" ]]; then
+    rm "${dst}"
+    if [[ -e "${backup}" ]]; then
+      mv "${backup}" "${dst}"
+    fi
+  fi
 done
