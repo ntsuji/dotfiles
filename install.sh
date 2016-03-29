c49b943f1fee13467ab0871192b1fdb066aa645a
Natsuo Tsuji <n.tsuji@gmail.com>
2016-03-29 20:13:26 +0900

    Fix scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 cd "$(dirname "${0}")"
-
-for symlink in $(ls | grep "\.symlink$"); do
-  readonly dotfile="$(echo ${symlink} | sed 's/\(.*\)\.symlink/.\1/')"
-  readonly src="${PWD}/${symlink}"
-  readonly dst="${HOME}/${dotfile}"
-  readonly backup="${HOME}/${dotfile}.bak"
+for symlink in $(ls -d *.symlink); do
+  dotfile=".${symlink%%.*}"
+  src="${PWD}/${symlink}"
+  dst="${HOME}/${dotfile}"
+  backup="${HOME}/${dotfile}.bak"
   if [[ -e "${dst}" &&
       ! -e "${backup}" &&
       "$(readlink "${dst}")" != "${src}" ]]; then
